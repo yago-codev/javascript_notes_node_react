1c07c3d797baf097d485e1ba2f2c980986c31c68
yago-codev <yagomilano92@gmail.com>
2020-03-19 02:05:08 -0300

    Habilitando o cors

--- api/app.js
+++ api/app.js
@@ -1,16 +1,16 @@
-var express = require('express');
+const express = require('express');
 const app = express();
-var path = require('path');
-var logger = require('morgan');
+const path = require('path');
+const cors = require('cors');
+
 require('./src/config/database');
 
 const usersRouter = require('./src/app/routes/users');
 const notesRouter = require('./src/app/routes/notes');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
