6674d047407f5864e781413e8383c3dba4f5a37b
yago-codev <yagomilano92@gmail.com>
2020-03-19 02:41:58 -0300

    Estruturando o projeto, criando estilização global, configurando o bulma/rbx

---
+++ front_react/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Notification, Section } from 'rbx';
+import './App.scss';
+
+const App = () => (
+  <>
+    <Section>
+      <Notification>
+        Javascript Notes
+      </Notification>
+    </Section>
+  </>
+);
+
+export default App;
