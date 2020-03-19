4e52d130c057faaf83f7b6224d44d02e70af83f5
yago-codev <yagomilano92@gmail.com>
2020-03-19 01:36:11 -0300

    Criando rota e funcionalidades no controller  para edição/deleção das notas

--- api/src/app/controllers/note.js
+++ api/src/app/controllers/note.js
@@ -43,4 +43,42 @@ module.exports = {
       res.status(500).json({ error: error });
     }
   },
+  edit: async(req, res) => {
+    const { title, body } = req.body;
+    const { id } = req.params;
+    
+    try {
+      let note = await Note.findById(id);
+
+      if ( isOwner(req.user, note) ) {
+        let note = await Note.findOneAndUpdate(id, 
+          { $set: { title, body } }, // campos que serão atualizados de acordo com os dados vindos do req.body
+          { upsert: true, 'new': true } // config para retornar a nota atualizada
+        );
+
+        res.json(note);
+      } else {
+        res.status(403).json({ error: 'Permission denied!' });
+      }
+    }
+    catch (error) {
+      res.status(500).json({ error: 'Problem to update a note!' });
+    }
+  },
+  delete: async(req, res) => {
+    const { id } = req.params; // pegando o id da url
+    try {
+      let note = await Note.findById(id);
+
+      if ( isOwner(req.user, note) ) {
+        await note.delete();
+        res.status(204).json({ message: 'Your note has been deleted with success!' });
+      } else {
+        res.status(403).json({ error: 'Permission denied!' });
+      }
+    }
+    catch (error) {
+      res.status(500).json({ error: 'Problem to delete a note!' });
+    }
+  },
 }
--- api/src/app/routes/notes.js
+++ api/src/app/routes/notes.js
@@ -5,6 +5,9 @@ const note = require('../controllers/note');
 const withAuth = require('../middlewares/auth');
 
 router.post('/', withAuth, note.create);
+router.get('/', withAuth, note.readAll);
 router.get('/:id', withAuth, note.read);
+router.put('/:id', withAuth, note.edit);
+router.delete('/:id', withAuth, note.delete);
 
 module.exports = router;
