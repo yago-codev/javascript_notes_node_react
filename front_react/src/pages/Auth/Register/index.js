64ca5eff0b847a50cc0de4713b725259ebe60af8
yago-codev <yagomilano92@gmail.com>
2020-03-23 23:18:56 -0300

    Estruturando/estilizando a página de registro de usuários

--- front_react/src/pages/Auth/Register/index.js
+++ front_react/src/pages/Auth/Register/index.js
@@ -1,8 +1,43 @@
 import React from 'react';
+import { Column, Section, Title, Container, Card } from 'rbx';
+
+import Header from '../../../components/Header';
+
+import logoImage from '../../../assets/images/logo.png';
+
+import './index.scss';
 
 const Register = () => (
   <>
-    Hello World
+    <Header />
+
+    <Section size="medium" className="auth">
+      <Container>
+        <Column.Group centered>
+          <Column size={3}>
+            <Card>
+              <Card.Content>
+                <Section>
+                  <Column.Group centered>
+                    <Column size={12}>
+                      <img src={logoImage} />
+                    </Column>
+                  </Column.Group>
+
+                  <Column.Group>
+                    <Column size={12}>
+                      <Title size={6} className="has-text-grey has-text-centered">
+                        Suas notas na nuvem
+                      </Title>
+                    </Column>
+                  </Column.Group>
+                </Section>
+              </Card.Content>
+            </Card>
+          </Column>
+        </Column.Group>
+      </Container>
+    </Section>
   </>
 )
 
